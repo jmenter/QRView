f4aa65a07a033406245e59528c71da02008d2df3
Jeff Menter <jmenter@gmail.com>
2018-04-16 20:37:04 -0500

    as simple as she gets

--- QRView.m
+++ QRView.m
@@ -3,23 +3,15 @@
  
  Copyright (c) 2018 Jeff Menter
  
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #import "QRView.h"
@@ -27,29 +19,14 @@
 
 @implementation QRView
 
-- (void)setMessage:(NSString *)message;
-{
-    if ([message isEqualToString:_message]) { return; }
-    
-    _message = message.copy;
-    [self setNeedsDisplay];
-}
-
-- (void)setCorrectionLevel:(NSString *)correctionLevel;
-{
-    if ([correctionLevel isEqualToString:_correctionLevel]) { return; }
-
-    _correctionLevel = correctionLevel.copy;
-    [self setNeedsDisplay];
-}
+- (void)setMessage:(NSString *)message; { _message = message.copy; [self setNeedsDisplay]; }
+- (void)setCorrectionLevel:(NSString *)correctionLevel; { _correctionLevel = correctionLevel.copy; [self setNeedsDisplay]; }
 
 - (UIImage *)qrImage;
 {
     CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
     [filter setValue:[self.message ?: @"" dataUsingEncoding:NSISOLatin1StringEncoding] forKey:@"inputMessage"];
-    [filter setValue:([self.correctionLevel.uppercaseString isEqualToString:@"H"] ||
-                      [self.correctionLevel.uppercaseString isEqualToString:@"L"] ||
-                      [self.correctionLevel.uppercaseString isEqualToString:@"Q"]) ? self.correctionLevel.uppercaseString : @"M" forKey:@"inputCorrectionLevel"];
+    [filter setValue:([@[@"H", @"L", @"Q"] containsObject:self.correctionLevel.uppercaseString]) ? self.correctionLevel.uppercaseString : @"M" forKey:@"inputCorrectionLevel"];
     return [UIImage imageWithCIImage:filter.outputImage];
 }
 
